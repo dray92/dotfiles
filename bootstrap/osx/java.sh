944029e59d0d8e94b88e169fea856a238881e696
Debosmit Ray <dray92@uw.edu>
2018-10-02 18:58:00 -0700

    added virtual-envs, java and updated some of the more basic installs

---
+++ bootstrap/osx/java.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# this is to get the latest java, which is not necessarily the tradition.
+# brew cask install java
+
+brew tap caskroom/versions
+brew cask install java8
+
+brew install maven gradle
+
+# this is very manual right now
+printf "\033[0;31mRun 'ls /Library/Java/JavaVirtualMachines/', pick latest jdk then run 'echo JAVA_HOME=/Library/Java/JavaVirtualMachines/{ENTER_JDK_NAME_HERE}/Contents/Home >> $HOME/.bash_profile' and source bash_profile.\033[0m\n"
