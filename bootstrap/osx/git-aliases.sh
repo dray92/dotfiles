2bec86a184ef2bf26f16ca8ec7442fc254cf9094
Debosmit Ray <dray92@uw.edu>
2021-05-20 18:30:43 -0700

    update bash profile

--- bootstrap/osx/git-aliases.sh
+++ bootstrap/osx/git-aliases.sh
@@ -13,6 +13,15 @@ alias gpull="git pull"
 alias gpush="git push"
 alias gl="git log --graph --pretty=format:'%C(blue)%ai %C(yellow)%H %C(green)%an %C(red)%s'"
 
+##
+#----------Git branch--------
+##
+parse_git_branch() {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+
+export PS1="\u@\h \W\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
+
 EOF
 }
 
