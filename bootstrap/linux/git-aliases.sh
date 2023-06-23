c9d4b6f291389c37019ed5f39cf0365e4c2683f5
Debosmit Ray <dray92@uw.edu>
2023-06-23 19:09:59 +0000

    fix gpusho

--- bootstrap/linux/git-aliases.sh
+++ bootstrap/linux/git-aliases.sh
@@ -4,6 +4,13 @@ _install_git_aliases () {
 	sudo printf "##\n#------Git alias preferences------\n##\n" >> $HOME/.bashrc
 
 	cat <<EOF >> $HOME/.bashrc
+##
+#----------Git branch--------
+##
+parse_git_branch() {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/'
+}
+
 alias gst="git status"
 alias ga="git add"
 alias gc="git commit"
@@ -12,14 +19,8 @@ alias gb="git branch"
 alias gpull="git pull"
 alias gpush="git push"
 alias gl="git log --graph --pretty=format:'%C(blue)%ai %C(yellow)%H %C(green)%an %C(red)%s'"
-alias gpusho="git push origin \$(git branch --show-current)"
+alias gpusho="git push origin \$(parse_git_branch)"
 
-##
-#----------Git branch--------
-##
-parse_git_branch() {
-  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
-}
 
 export PS1="\u@\h \W\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
 
