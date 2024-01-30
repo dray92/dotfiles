6b83687f62dfd9a5f5af0ef66222dc2134215a0e
Debosmit Ray <dray92@uw.edu>
2024-01-30 10:40:46 -0800

    Update basics.sh

--- bootstrap/linux/basics.sh
+++ bootstrap/linux/basics.sh
@@ -10,6 +10,11 @@ alias curr_ip="curl -s http://checkip.dyndns.org/ | sed 's/[a-zA-Z<>/ :]//g'"
 
 
 EOF
+	sudo apt-get install -y fontconfig || sudo apt-get update && sudo apt-get install -y fontconfig
+        curl -sS https://raw.githubusercontent.com/diogocavilha/fancy-git/master/install.sh | sh
+        bash -i -c 'fancygit --suggested-global-git-config-apply'
+        bash -i -c 'fancygit --theme-human'
+        bash -i -c 'fancygit --enable-host-name'
 }
 
 _install_basics
